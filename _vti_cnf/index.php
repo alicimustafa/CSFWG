20f993284f209f3c5058389b5b553d67860da40c
mustafa alici <alicimustafadev@yahoo.com>
2015-11-03 11:25:54 -0700

    Just the begining

---
+++ _vti_cnf/index.php
@@ -0,0 +1,17 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|mustafa-PC\\mustafa
+vti_modifiedby:SR|mustafa-PC\\mustafa
+vti_timelastmodified:TR|03 Nov 2015 18:19:47 -0000
+vti_timecreated:TR|03 Nov 2015 18:15:58 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TR|03 Nov 2015 18:15:58 -0000
+vti_cacheddtm:TX|03 Nov 2015 18:19:47 -0000
+vti_filesize:IR|310
+vti_cachedbodystyle:SR|<body>
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_metatags:VR|HTTP-EQUIV=Content-Type text/html;\\ charset=utf-8
+vti_charset:SR|utf-8
